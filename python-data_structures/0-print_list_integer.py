d443f3ec40e8295fe315523f94756ff21e4f0101
root <Collinsanyona66@gmail.comgit config --global user.email Collinsanyona66@gmail.comcd>
2023-11-06 03:12:37 -0800

    0-print_list_integer.py

--- python-data_structures/0-print_list_integer.py
+++ python-data_structures/0-print_list_integer.py
@@ -1,6 +1,6 @@
 #!/usr/bin/python3
 my_list = [1, 2, 3, 4, 5]
-def print_list_integer(my_list=[]):
+def print_list_integer():
     for i in my_list:
         print("{}".format(i))
-print_list_integer(my_list=[])
+print_list_integer()
