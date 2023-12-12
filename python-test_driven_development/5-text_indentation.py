c5eecc82a8ad9a57638cd4042746120897011992
CollinsAnyona <collinsanyona66@gmail.com>
2023-12-12 02:13:04 -0800

    5-text_indentation.py

--- python-test_driven_development/5-text_indentation.py
+++ python-test_driven_development/5-text_indentation.py
@@ -1,50 +1,27 @@
 #!/usr/bin/python3
-""""Doc"""
+''' Defining a text indentation function that prints indented text '''
 
 
 def text_indentation(text):
-    """ Doc here
-    """
+    '''
+    Prints a text with 2 new lines after . ? and :
+    Ensures text is a string
+    Raises an error if text is not a string
+    '''
     if type(text) is not str:
         raise TypeError("text must be a string")
-
-    for delim in ".:?":
-        text = (delim + "\n\n").join(
-            [line.strip(" ") for line in text.split(delim)])
-
-    print("{}".format(text), end="")
-
-# def text_indentation(text):
-#     """"Doc"""
-#     if not isinstance(text, (str,)):
-#         raise TypeError("text must be a string")
-
-#     characters = [".", "?", ":"]
-#     index = 0
-#     while index < len(text):
-#         if text[index] in characters:
-#             print(text[index], end="")
-#             print('\n')
-#             index += 1
-#             if text[index] == " ":
-#                 index += 1
-#         else:
-#             print(text[index], end="")
-#             index += 1
-
-# def text_indentation(text):
-#     """"Doc"""
-#     if not isinstance(text, (str,)):
-#         raise TypeError("text must be a string")
-
-#     characters = [".", "?", ":"]
-#     for i in range(len(text)):
-#         if text[i] in characters:
-#             try:
-#                 if text[i + 1] == " ":
-#                     text = text[:i + 1] + "\n\n" + text[i + 2:]
-#                 else:
-#                     text = text[:i + 1] + "\n\n" + text[i + 1:]
-#             except IndexError:
-#                 pass
-#     print(text, end="")
+    for i in range(0, len(text)):
+        if text[i] in [".", "?", ":"]:
+            print(text[i], end="")
+            print("\n")
+        elif text[i] == " ":
+            for x in range(i, 0, -1):
+                if text[x] == " ":
+                    continue
+                elif text[x] in [".", "?", ":"]:
+                    break
+                else:
+                    print(text[i], end="")
+                    break
+        else:
+            print(text[i], end="")
