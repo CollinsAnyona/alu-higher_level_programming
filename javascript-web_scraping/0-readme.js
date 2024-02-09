9fe3ec39b73d8347b501984fa500f7a073bcb7ee
root <Collinsanyona66@gmail.comgit config --global user.email Collinsanyona66@gmail.comcd>
2024-02-09 01:15:50 -0800

    0-readme.js

--- javascript-web_scraping/0-readme.js
+++ javascript-web_scraping/0-readme.js
@@ -1,6 +1,8 @@
 #!/usr/bin/node
 const fs = require('fs');
-fs.readFile(process.argv[2], (error, paragraph) => {
-  if (error) throw error;
-  console.log(paragraph.toString());
+
+fs.readFile(process.argv[2], function (err, data) {
+  err && console.log(err);
+  const content = data;
+  console.log(content.toString());
 });
