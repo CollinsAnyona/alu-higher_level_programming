304387cf7409cf6156caae5fd46b5c06bfc0e483
root <Collinsanyona66@gmail.comgit config --global user.email Collinsanyona66@gmail.comcd>
2024-02-13 01:33:39 -0800

    100-map.js

--- javascript_objects_scopes_closures/101-sorted.js
+++ javascript_objects_scopes_closures/101-sorted.js
@@ -1,14 +1,11 @@
 #!/usr/bin/node
-
 const dict = require('./101-data.js').dict;
 const newDict = {};
 for (const key in dict) {
-  const value = dict[key];
-  if (newDict[value] === undefined) {
-    newDict[value] = [];
-    newDict[value].push(key);
+  if (newDict[dict[key]] === undefined) {
+    newDict[dict[key]] = [key];
   } else {
-    newDict[value].push(key);
+    newDict[dict[key]].push(key);
   }
 }
 console.log(newDict);
